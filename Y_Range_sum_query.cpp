17ce7978bc87bf70d3f8c4cd027c438e2ce31da5
Alif Riasat <alifborno@gmail.com>
2025-08-30 04:00:52 +0600

    Add files via upload

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,q;
+    cin>>n>>q;
+    vector<int>a(n+1),pre(n+1);
+    for(int i=1;i<a.size();i++){
+        cin>>a[i];
+    }
+
+    pre[1]=a[1];
+    for(int i=2;i<=n;i++){
+        pre[i]=pre[i-1]+a[i];
+    }
+
+    while(q--){
+        int l,r,sum=0;
+        cin>>l>>r;
+        if(l==1){
+            sum=pre[r];
+        }
+        else{
+            sum=pre[r]-pre[l-1];
+        }
+        cout<<sum<<endl;
+    }
+}
