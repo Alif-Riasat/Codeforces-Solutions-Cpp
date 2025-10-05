3548912ccc418a2782d4061e44f58ee5e1af9044
Alif Riasat <alifborno@gmail.com>
2025-10-05 21:49:40 +0600

    Add files via upload

---
+++ B_Bogosort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        reverse(a.begin(),a.end());
+        for(int x: a){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
